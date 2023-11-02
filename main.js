c4210e6c051542857de7acd852792ae8f45e8ba2
Johann JEANNELLE <johann@cher-ami.tv>
2023-11-02 16:35:13 +0100

    working fine

--- main.js
+++ main.js
@@ -1,89 +1,203 @@
 import "./style.less";
+import { DragGesture } from "@use-gesture/vanilla"
+import gsap from "gsap";
 
-const numberRow = 5;
-const numberItembyRow = 5;
-const numberCircle = numberRow * numberItembyRow;
-const cercleSize = 400;
+// Params
+const numberRow = 10;
+const numberItembyRow = 10;
 
-let posX = 0;
-let posY = 0;
-let startX = 0;
-let startY = 0;
-let mouseX = 0;
-let mouseY = 0;
-let oldOffsetX = 0;
-let oldOffsetY = 0;
-let offsetX = 0;
-let offsetY = 0;
+let currentIdx = Math.floor((numberRow*numberItembyRow) / 2) + Math.floor((numberItembyRow) / 2);
 
-const windowW = window.innerWidth;
-const windowH = window.innerHeight;
+// Sizes
+let windowW = null;
+let windowH = null;
+let cercleSize = null;
+let menuW = null;
+let menuH = null;
 
-const menuW = cercleSize*numberItembyRow;
-const menuH = cercleSize*numberRow;
+// Posititon
+let menuPosition = {x: 0, y: 0}
+let dragValue = {x: 0, y: 0}
+let limitPosition = { min: {x: null, y: null}, max: {x: null, y: null} }
 
-const Wrapper = document.querySelector(".Menu_wrapper")
+let gesture = null;
+
+// Dom Element
+let wrapperMenu = document.querySelector(".Menu_wrapper");
+let rowsMenu = document.getElementsByClassName("Menu_row");
+let itemsMenu = document.getElementsByClassName("Menu_item");
 
 initMenu();
 
 function initMenu() {
-  Wrapper.style.transform = `translate(0px, 0px)`
+  setTimeout(() => {
+    createMenu();
+    setMenu();
+    bindDragGesture();
+  }, "1000");
+}
 
-  // Create circle
-  for (let i = 0; i < numberRow; i++) {
-    const row = document.createElement('div');
-    row.className = 'Menu_row';
-    
+function createMenu() {
+
+  for (let idx = 0; idx < numberRow; idx++) {
+    const row = document.createElement("div");
+    row.className = "Menu_row";
     for (let i = 0; i < numberItembyRow; i++) {
-      const item = document.createElement('div');
-      item.className = 'Menu_item';
-      item.style.width = cercleSize+'px';
-      item.style.height = cercleSize+'px';
-      item.className = 'Menu_item';
-      item.textContent = i;
-      row.appendChild(item); 
+      const item = document.createElement("div");
+      item.className = "Menu_item";
+      item.style.backgroundImage = `url(https://picsum.photos/id/${idx + 1}${i + 1}/${300}/${300})`;
+      item.className = "Menu_item";
+      row.appendChild(item);
     }
-    Wrapper.appendChild(row); 
+    wrapperMenu.appendChild(row);
   }
-
-  // Set size & position
-  Wrapper.style.width = menuW+'px';
-  Wrapper.style.height = menuH+'px';
-  const currentBound = document.getElementsByClassName("Menu_item")[12].getBoundingClientRect();
-  posX = (windowW/2) - (currentBound.x+(currentBound.width/2));
-  posY = (windowH/2) - (currentBound.y+(currentBound.height/2));
-  Wrapper.style.transform = `translate(${posX}px, ${posY}px)`
 }
 
-window.addEventListener("mousedown", handleClick);
+function setMenu() {
+  windowW = window.innerWidth;
+  windowH = window.innerHeight;
 
-function handleClick(e) {
-  window.addEventListener("mousemove", handleMouse);
-  window.addEventListener("mouseup", handleRelease);
-  startX = e.clientX;
-  startY = e.clientY;
-  oldOffsetX = 0;
-  oldOffsetY = 0;
-  offsetX = 0;
-  offsetY = 0;
-  Wrapper.style.cursor = "grabbing";
+  cercleSize = windowW/3;
+  menuW = cercleSize * numberItembyRow + cercleSize / 2;
+  menuH = cercleSize * numberRow;
+
+  wrapperMenu.style.width = menuW + "px";
+  wrapperMenu.style.height = menuH + "px";
+
+  setMaxLeftPosition();
+
+  Array.prototype.forEach.call(itemsMenu, function(item) {
+    item.style.width = cercleSize + "px";
+    item.style.height = cercleSize + "px";
+  })
+
+  Array.prototype.forEach.call(rowsMenu, function(row, index) {
+    if (index % 2 == 0) {
+      row.style.marginLeft = cercleSize / 2 + "px";
+    }
+  })
+
+  const newValues = centerToIndex(currentIdx);
+  updateSliderPosition(newValues, 0)
+  menuPosition = newValues
+  dragValue = menuPosition
 }
 
-function handleMouse(e) {
-  mouseX = e.clientX;
-  mouseY = e.clientY;
-  offsetX = oldOffsetX + mouseX - startX;
-  offsetY = oldOffsetY + mouseY - startY;
-  Wrapper.style.transform = `translate(${posX+offsetX}px, ${posY+offsetY}px)`
+function bindDragGesture() {
+  gesture = new DragGesture(wrapperMenu, onDragHandler)
 }
 
-function handleRelease() {
-  window.removeEventListener("mouseup", handleRelease);
-  window.removeEventListener("mousemove", handleMouse);
-  posX = posX+offsetX
-  posY = posY+offsetY
-  Wrapper.style.cursor = "grab";
+function setMaxLeftPosition() {
+  limitPosition.max.x = (windowW/2) - (cercleSize/2);
+  limitPosition.max.y = (windowH/2) - (cercleSize/2);
+  limitPosition.min.x = (windowW/2) - menuW + (cercleSize/2);
+  limitPosition.min.y = (windowH/2) - menuH + (cercleSize/2);
 }
+
+function onDragHandler(event) {
+  const active = event.active
+
+  if (active) {
+    dragValue.x += event.delta[0] * 1.5
+    dragValue.y += event.delta[1] * 1.5
+
+    const menuTranslation = getMenuPositionByDragValue(dragValue)
+    menuPosition = menuTranslation
+    updateSliderPosition(menuTranslation, 0)
+  }else {
+    const newValues = findNearestIdx();
+    updateSliderPosition(newValues)
+    menuPosition = newValues
+    dragValue = menuPosition
+  }
+}
+
+function findNearestIdx() {
+  const list = getDistList()
+  const index = list.indexOf(Math.min.apply( Math, list ))
+  return centerToIndex(index)
+}
+
+function centerToIndex(index) {
+  currentIdx = index
+
+  const rowIdx = Math.floor(index / numberItembyRow) + 1
+  const caseIdx = index - ((rowIdx - 1)*numberItembyRow) + 1
+  const marge = (rowIdx - 1) % 2 == 0 ? cercleSize/2 : 0
+
+  return {
+    x: (windowW/2) - caseIdx*cercleSize + cercleSize/2 - marge,
+    y: (windowH/2) - rowIdx*cercleSize + cercleSize/2,
+  }
+}
+
+function getMenuPositionByDragValue(values) {
+  return {
+    x: clamp(limitPosition.min.x, values.x, limitPosition.max.x),
+    y: clamp(limitPosition.min.y, values.y, limitPosition.max.y),
+  }
+}
+
+function getDistList() {
+  const distList = [];
+  Array.prototype.forEach.call(itemsMenu, function(item) {
+    const itemBound = item.getBoundingClientRect();
+    const dx = (itemBound.x+(itemBound.width/2)) - windowW/2;
+    const dy = (itemBound.y+(itemBound.height/2)) - windowH/2;
+    const dist = Math.sqrt(dx * dx + dy * dy);
+    distList.push(dist)
+  });
+  return distList
+}
+
+function updateSliderPosition(values, duration = 0.5) {
+  gsap.killTweensOf(wrapperMenu)
+  gsap.to(wrapperMenu, {
+    x: values.x,
+    y: values.y,
+    duration,
+    ease: "power3.out",
+  })
+}
+
+function raf() {
+  const list = getDistList()
+
+  const nearestID = list.indexOf(Math.min.apply( Math, list ))
+
+  list.forEach((dist, idx) => {
+
+    let scale = .5
+    const diff = (window.innerWidth / 2) - dist
+
+    if(diff >= 0) {
+      const sup = ((diff * 50) / (window.innerWidth / 2))
+      scale = scale + (sup/100)
+    }
+
+    itemsMenu[idx].classList.remove('active')
+    if(idx === nearestID) {
+      itemsMenu[idx].classList.add('active')
+    }
+
+    itemsMenu[idx].style.transform = `scale(${scale})`;
+  })
+
+  window.requestAnimationFrame(raf);
+}
+
+function onResizeHandler() {
+  setMenu()
+}
+
+window.requestAnimationFrame(raf);
+window.addEventListener("resize", onResizeHandler)
+
+// Utils
+const clamp = (min, num, max) => Math.min(Math.max(num, min), max);
+
+
+
 
 
 
